103548a59df33fee998de8e5df4b24c03a920d82
Jiancong Zheng <jiancong0204@outlook.com>
2021-10-05 23:18:01 +0800

    add third party for gflags

--- third_party/third_party.cmake
+++ third_party/third_party.cmake
@@ -4,5 +4,12 @@ set(GLOG_ROOT_DIR "${PROJECT_SOURCE_DIR}/third_party/glog")
 set_target_properties(third_party_glog PROPERTIES
   IMPORTED_LOCATION "${GLOG_ROOT_DIR}/lib/libglog.so"
   INTERFACE_INCLUDE_DIRECTORIES "${GLOG_ROOT_DIR}/include"
-  INTERFACE_LINK_LIBRARIES "-lpthread"
+)
+
+# gflags
+add_library(third_party_gflags STATIC IMPORTED)
+set(GFLAGS_ROOT_DIR "${PROJECT_SOURCE_DIR}/third_party/gflags")
+set_target_properties(third_party_gflags PROPERTIES
+  IMPORTED_LOCATION "${GFLAGS_ROOT_DIR}/lib/libgflags.a"
+  INTERFACE_INCLUDE_DIRECTORIES "${GFLAGS_ROOT_DIR}/include"
 )
